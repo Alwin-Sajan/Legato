d97d387fec97fc11853c00c3aa9c39044b667ea0
Alwin-Sajan <alwinsajan225@gmail.com>
2024-06-24 23:01:51 +0530

    add demo section and bg

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,39 @@ import Reveal from './components/Reveal';
 
 const App = () => {
   const [darkmode, setdarkmode] = useState(true);
+  const [demo, setDemo] = useState(false);
   const toggleDarkMode = () => {
     setdarkmode(prevMode => !prevMode);
   }
   return (
     <div className={`w-full overflow-hidden relative h-auto min-h-screen ${darkmode ? 'bg-black text-white ' : 'bg-white text-black '}`}>
-      <img src={darkmode ? "https://img.freepik.com/free-vector/hexagonal-black-background-modern-design_1017-37442.jpg" : back} className='fixed bg-slate-500 opacity-60 w-full z-20' alt="" />      
-      <div className='relative z-30'>
+      <img src={darkmode ? "https://img.freepik.com/free-vector/hexagonal-black-background-modern-design_1017-37442.jpg" : back} className='fixed bg-slate-500 opacity-60 w-full z-20' alt="" />
+
+      <div className={`ml-[10vw] mt-[10vh] rounded w-[80vw] h-[80vh] glass fixed z-50 ${demo ? "block" : 'hidden'}`}>
+        <div className='flex h-full p-8 relative'>
+          <h3 className='text-4xl relative text-black'>Demo Video</h3>
+          <button type="button" className='bg-[red] absolute right-1 h-16 p-4 rounded mx-60' onClick={() => setDemo(!demo)}>X</button>
+        </div>
+      </div>
+      <div className={`${demo ? "opacity-80 " : ""} relative z-30`}>
 
         <Navbar darkMode={darkmode} toggleDarkMode={toggleDarkMode} />
         <div className='grid justify-center h-[50vh] pt-36 font-semibold text-7xl'>
-        <Reveal>Legato</Reveal>
+          <Reveal>Legato</Reveal>
         </div>
 
         <Features darkmode={darkmode} />
 
         <div className='h-[90vh] bg-gradient-to-r from-cyan-500 to-pink-500'>
           <h1 className='pl-16 text-5xl font-bold mb-0 pt-16'>About Us</h1>
+          <button type="button" className='bg-[red] my-16 rounded p-4 mx-60' onClick={() => setDemo(!demo)}>Demo</button>
         </div>
 
         <Team />
         <Footer />
 
       </div>
-      
+
     </div>
   )
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,17 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Signup = () => {
     return (
-        <div class="m-auto h-[25rem] w-[23rem] bg-black text-gray-200 my-[8rem] rounded-xl">
-            <h1 class="pb-5 text-center text-3xl font-bold pt-10">Sign up</h1>
-            <div class="mx-auto w-[18rem]">
-                <div class=" flex  flex-col">
-                    <input type="text" name="" id="" placeholder="username" class="border-b-2 border-gray-500 bg-transparent focus:outline-none" />
-                    <input type="text" name="" id="" placeholder="Email" class="border-b-2 border-gray-500 bg-transparent focus:outline-none my-5" />
-                    <input type="password" name="" id="" placeholder="password" class="border-b-2 border-gray-500 bg-transparent mb-5 focus:outline-none" />
-                </div>
-                <div class="mt-5  text-center">
-                    <button class="border px-9 py-[0.2rem] font-semibold rounded-sm hover:bg-slate-50 hover:text-black">SIGN UP</button>
-                    <p class="py-5">Already have a account?<Link to="/signin">Sign in</Link></p>
+        <div className="h-screen w-full grid justify-center py-[4rem] bg-gradient-to-r from-cyan-500 to-pink-500">
+            <Link to="/" className='text-center text-3xl text-white font-semibold mb-4'>Legato</Link>
+            <div class="m-auto h-[25rem] w-[23rem] bg-black text-gray-200 rounded-xl">
+                <h1 class="pb-5 text-center text-3xl font-bold pt-10">Sign up</h1>
+                <div class="mx-auto w-[18rem]">
+                    <div class=" flex  flex-col">
+                        <input type="text" name="" id="" placeholder="Username" class="border-b-2 border-gray-500 bg-transparent focus:outline-none" />
+                        <input type="text" name="" id="" placeholder="Email" class="border-b-2 border-gray-500 bg-transparent focus:outline-none my-5" />
+                        <input type="password" name="" id="" placeholder="Password" class="border-b-2 border-gray-500 bg-transparent mb-5 focus:outline-none" />
+                    </div>
+                    <div class="mt-5  text-center">
+                        <button class="border px-9 py-[0.2rem] font-semibold rounded-sm hover:bg-slate-50 hover:text-black">SIGN UP</button>
+                        <p class="py-5">Already have a account?<Link to="/signin">Sign in</Link></p>
+                    </div>
                 </div>
             </div>
         </div>
