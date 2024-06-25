af8f225ce362e8bdfc42d80d2bee083d26ef3713
Alwin-Sajan <alwinsajan225@gmail.com>
2024-06-25 23:23:34 +0530

    added about section

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ const App = () => {
           <h3 className='text-4xl relative text-black'>Demo Video</h3>
           <button type="button" className='bg-[red] absolute right-1 h-16 p-4 rounded mx-60' onClick={() => setDemo(!demo)}>X</button>
         </div>
-        <iframe width="980" height="360" className='mx-12 rounded mt-4'
+        <iframe width="w-full" height="360" className='mx-12 rounded mt-4'
           src="https://www.youtube.com/embed/tgbNymZ7vqY">
         </iframe>
       </div>
-      
+
       <div className={`${demo ? "opacity-80 " : ""} relative z-30`}>
 
         <Navbar darkMode={darkmode} toggleDarkMode={toggleDarkMode} />
@@ -36,10 +36,56 @@ const App = () => {
 
         <Features darkmode={darkmode} />
 
-        <div className='h-[90vh] bg-gradient-to-r from-cyan-500 to-pink-500'>
-          <h1 className='pl-16 text-5xl font-bold mb-0 pt-16'>About Us</h1>
-          <button type="button" className='bg-[red] my-16 rounded p-4 mx-60' onClick={() => setDemo(!demo)}>Demo</button>
+        {/* h-[90vh] */}
+        <div className=' bg-gradient-to-r pb-16 from-cyan-500 to-pink-500'>
+          <h1 className='pl-16 text-5xl font-bold  mb-0 pt-16'>About Legato: Your Campus Connection Hub</h1>
+
+          <div className='px-32 pt-8'>
+
+            <p>
+              Legato is your one-stop platform for everything happening on campus! We connect you to the vibrant college
+              community by bringing all your clubs, events, and achievements together in one central space.
+
+            </p>
+
+            <div className='flex'>
+              <div className='pr-16'>
+                <h3 className='text-2xl py-4 font-medium'>Here's what makes Legato unique:</h3>
+
+                <ul className='pl-4 gap-4 grid'>
+
+                  <li><strong>Centralized Event Hub:</strong> No more searching through endless emails or social media groups. Legato provides a comprehensive calendar of all club events and college-wide activities, making it easier than ever to discover and participate in what interests you.</li>
+                  <li><strong>Club Networking:</strong> Interact with different clubs and organizations. Legato allows you to explore clubs, learn about their missions, and connect with their members directly through the platform.</li>
+                  <li><strong>Community Building:</strong> Stay informed about college-wide events and initiatives. Legato fosters a sense of community by showcasing all the exciting happenings on campus, making you feel more connected to the college spirit.</li>
+                  <li><strong>Achievement Showcase:</strong> Proud of your accomplishments? Share your academic achievements, awards, or club milestones on Legato. Celebrate your success and inspire others!</li>
+                </ul>
+                </div>
+
+                <img src="https://thumbs.dreamstime.com/b/community-concept-pictogram-showing-figures-happy-family-84451213.jpg" className='w-60 h-60 mt-20 ' alt="community image" />
+            </div>
+
+            <h3 className='text-xl py-4 font-medium'>With Legato, you can:</h3>
+
+            <ul className='pl-4 gap-2 grid'>
+
+              <li>Browse upcoming events and RSVP with ease.</li>
+              <li>Discover new clubs and organizations that align with your interests.</li>
+              <li>Connect with fellow students who share your passions.</li>
+              <li>Stay updated on important college announcements and initiatives.</li>
+              <li>Share your achievements and gain recognition from the college community.</li>
+
+            </ul>
+
+            <p className='py-4'>
+              Legato is your gateway to a more engaged and fulfilling college experience. We empower you to explore your interests, connect with like-minded individuals, and contribute to the vibrant campus life.
+            </p>
+            <strong>Join Legato today and become an active part of your college community!</strong>
+
+
+          </div>
         </div>
+
+        <button type="button" className='bg-[red] my-16 rounded p-4 mx-60' onClick={() => setDemo(!demo)}>Demo</button>
 
         <Team />
         <Footer />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from "framer-motion"
 import { Link } from 'react-router-dom'
 
 const Navbar = ({ toggleDarkMode, darkmode }) => {
+    const [displayMenu, setDisplayMenu] = useState(false);
 
     return (
         <div className='flex fixed  bg-gradient-to-r from-cyan-500 to-pink-500 w-full justify-center items-center z-50 py-6 '>
-            <ul className='lg:flex items-center text-lg gap-16'>
+            <button className={` lg:hidden absolute top-4 `} onClick={() => { setDisplayMenu(!displayMenu) }}>Menu</button>
+            <ul className={`${displayMenu ? "mt-10" : "max-md:hidden"} lg:flex max-md:grid items-center justify-center text-lg lg:gap-16`}>
                 <li className='mr-60'><a href="">Legato</a></li>
                 <li><a href="">Home</a></li>
                 <li><a href="#about">About Us</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><button onClick={toggleDarkMode} ><img className='w-8 ml-40 bg-white rounded-full' src="https://www.svgrepo.com/show/309493/dark-theme.svg" alt="" /></button></li>
+                <li><button onClick={toggleDarkMode} ><img className='w-8 lg:ml-40 bg-white rounded-full' src="https://www.svgrepo.com/show/309493/dark-theme.svg" alt="" /></button></li>
                 <li><button className='hover:bg-slate-800 border-white border-2 px-4 rounded-2xl outline-none py-1'><Link to="/signin">Join us</Link> </button></li>
             </ul>
         </div>
